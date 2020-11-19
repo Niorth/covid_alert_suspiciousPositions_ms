4c5720d82b5cd91ada390d99dad92cf1b2e41e63
GuillaumeGT <=>
2020-11-20 00:03:18 +0100

    Begin isSuspicious

--- src/main/java/fr/projetiwa/SuspiciousPosition/controllers/SuspiciousPositionController.java
+++ src/main/java/fr/projetiwa/SuspiciousPosition/controllers/SuspiciousPositionController.java
@@ -1,18 +1,31 @@
 package fr.projetiwa.SuspiciousPosition.controllers;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.projetiwa.SuspiciousPosition.Services.SuspiciousPositionService;
+import fr.projetiwa.SuspiciousPosition.models.Position;
+import fr.projetiwa.SuspiciousPosition.models.SuperClassPosition;
 import fr.projetiwa.SuspiciousPosition.models.SuspiciousPosition;
 import fr.projetiwa.SuspiciousPosition.repositories.SuspiciousPositionRepository;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
 import java.sql.Timestamp;
 import java.util.*;
 
@@ -23,6 +36,8 @@ public class SuspiciousPositionController {
     @Autowired
     KafkaConsumer<String,SuspiciousPosition> consumer;
 
+    @Autowired
+    private HttpServletRequest request;
 
 
     @Autowired
@@ -49,7 +64,57 @@ public class SuspiciousPositionController {
     }
     @PostMapping()
     public ResponseEntity<SuspiciousPosition> create(@RequestBody SuspiciousPosition sus) {
-        return new ResponseEntity<SuspiciousPosition>(suspiciousPositionService.saveAndFlush(sus), HttpStatus.CREATED);}
+        return new ResponseEntity<SuspiciousPosition>(suspiciousPositionService.saveAndFlush(sus), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    @RequestMapping("/isSuspicious")
+    public String isSuspicious(@RequestHeader (name="Authorization") String token) throws JsonProcessingException {
+
+        String payload = token.split("\\.")[1];
+
+        try{
+            String str = new String(Base64.decodeBase64(payload),"UTF-8");
+            JSONObject jsonObject = new JSONObject(str);
+            String personId = jsonObject.getString("sub");
+
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+
+
+        //Recuperation des positions
+        String urlPositions = "http://localhost:3003/positions";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.add(HttpHeaders.AUTHORIZATION, token);
+        //System.out.println(headers);
+        HttpEntity request = new HttpEntity(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String serviceUrl = urlPositions;
+        ResponseEntity<String> response = restTemplate.exchange( serviceUrl, HttpMethod.GET, request, String.class, 1 );
+        List<Position> positionList = new ObjectMapper().readValue(response.getBody(), new TypeReference<List<Position>>() {});
+
+        //System.out.println(positionList.toString());
+
+        //Recuperation des positions suspectes
+        List<SuspiciousPosition> suspiciousPositionList = suspiciousPositionService.findAll();
+        //List<SuperClassPosition> abc = (List<SuperClassPosition>)(SuspiciousPosition) suspiciousPositionList;
+        /*SuspiciousPosition sp = new SuspiciousPosition();
+        sp.setLatitude((float) 43.6016860000000000);
+        sp.setLongitude((float) 3.9114860000000000);
+        suspiciousPositionList.add(sp);*/
+        for(Position position : positionList){
+            System.out.println(suspiciousPositionList.contains(position));
+        }
+
+
+        //System.out.println(response.getBody());
+        //return "{\"success\":1}";
+        return response.getBody();
+    }
+
 
 
 }
