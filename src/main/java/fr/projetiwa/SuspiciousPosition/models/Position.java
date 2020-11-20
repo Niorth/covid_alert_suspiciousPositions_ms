9a73e1ebe7d4e70b8626b4bc2a58b28e7720bbac
GuillaumeGT <=>
2020-11-20 22:05:23 +0100

    Add accuracy to model

--- src/main/java/fr/projetiwa/SuspiciousPosition/models/Position.java
+++ src/main/java/fr/projetiwa/SuspiciousPosition/models/Position.java
@@ -3,13 +3,16 @@ package fr.projetiwa.SuspiciousPosition.models;
 
 import java.sql.Timestamp;
 
+
 public class Position extends SuperClassPosition {
 
     private String userId;
+    private float accuracy;
 
-    public Position(Float longitude, Timestamp positionDate, Float latitude, String userId) {
+    public Position(Float longitude, Timestamp positionDate, Float latitude, String userId, float accuracy) {
         super(longitude, positionDate, latitude);
         this.userId = userId;
+        this.accuracy = accuracy;
     }
 
     public Position() { }
@@ -22,8 +25,17 @@ public class Position extends SuperClassPosition {
         this.userId = userId;
     }
 
+    public float getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(float accuracy) {
+        this.accuracy = accuracy;
+    }
+
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
 }
+
